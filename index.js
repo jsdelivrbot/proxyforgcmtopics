6cde42208ffde465e1aaa3ec5ef4d8c70ed0433c
chamma75 <hamma_chabane@hotmail.com>
2018-02-10 14:20:24 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+
+app.set('port', port)
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
